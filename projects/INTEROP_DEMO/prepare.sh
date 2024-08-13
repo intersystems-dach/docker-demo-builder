0105f6aef0e89a2e8ff5b984f88f5b45359d145d
Andreas Schuetz <andreas.schuetz@intersystems.com>
2024-08-13 12:25:33 +0200

    Add functionality prepare script

---
+++ projects/INTEROP_DEMO/prepare.sh
@@ -0,0 +1,3 @@
+# Add commands here to prepare the environment for your 
+# iris application, e.g. install pyhton wheels.
+echo "Hello world!" > /home/"${ISC_PACKAGE_MGRUSER}"/hello.txt
--- resources/install_projects.sh
+++ resources/install_projects.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-find "/opt/irisbuild/projects" -maxdepth 2 -iname "iris.script" -print0 | 
+find "/home/${ISC_PACKAGE_MGRUSER}/irisbuild/projects" -maxdepth 2 -iname "iris.script" -print0 | 
 while IFS= read -r -d '' irisscript; do
     directory=$(dirname "$irisscript")
     echo "Found project: $(basename "$directory")"
+    if [ -f "$directory/prepare.sh" ]; then
+        echo "Found prepare.sh script"
+        cur_dir=$(pwd)
+        cd "$directory"
+        source "$directory/prepare.sh"
+        cd "$cur_dir"
+    fi
     iris session IRIS <<< $(cat $irisscript | sed "s|<#PROJECTDIR#>|$directory|g")
 done
 
-rm -r /opt/irisbuild/projects
+#rm -r /opt/irisbuild/projects
 
 exit
-
-
